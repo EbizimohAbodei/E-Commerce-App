8c7f968590f642306683b67fc208cdcbad6bf361
kendyvincent <vincentkendy@gmail.com>
2023-05-18 22:37:52 +0300

    Design footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+
+import { Paper, Typography, Box, Container } from "@mui/material";
 
 const Footer = () => {
   return (
-      <div className='footer'>
-          
-      
+    <div>
+      <Paper
+        sx={{
+          marginTop: "calc(10% + 60px)",
+          width: "100%",
+          position: "fixed",
+          bottom: 0,
+        }}
+        component="footer"
+        square
+        variant="outlined"
+      >
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: "center",
+              display: "flex",
+              my: 1,
+            }}
+          ></Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: "center",
+              display: "flex",
+              mb: 2,
+            }}
+          >
+            <Typography variant="caption" color="initial">
+              Copyright ©2023
+            </Typography>
+          </Box>
+        </Container>
+      </Paper>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
